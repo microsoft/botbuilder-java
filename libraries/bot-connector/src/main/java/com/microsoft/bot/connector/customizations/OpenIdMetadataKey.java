3fedc5df389e21f5b06710a2c11c950e1da42b3b
John Taylor <johtaylo@microsoft.com>
2018-04-02 10:41:33 -0700

    copyright headers and fix empty secirity for emulator

--- libraries/bot-connector/src/main/java/com/microsoft/bot/connector/customizations/OpenIdMetadataKey.java
+++ libraries/bot-connector/src/main/java/com/microsoft/bot/connector/customizations/OpenIdMetadataKey.java
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 package com.microsoft.bot.connector.customizations;
 
 import java.security.interfaces.RSAPublicKey;
