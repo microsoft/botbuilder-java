567b8429b9cb8715c032f8b521ca6753f6f30041
Francisco Ponce Gomez <v-frgome@microsoft.com>
2018-04-30 12:25:45 -0700

    Added necessary files

--- Generator/generator-botbuilder-java/generators/app/index.js
+++ Generator/generator-botbuilder-java/generators/app/index.js
@@ -2,6 +2,10 @@
 const Generator = require('yeoman-generator');
 const chalk = require('chalk');
 const yosay = require('yosay');
+const path = require('path');
+const _ = require('lodash');
+const extend = require('deep-extend');
+const mkdirp = require('mkdirp');
 
 module.exports = class extends Generator {
   prompting() {
@@ -11,12 +15,9 @@ module.exports = class extends Generator {
     );
 
     const prompts = [
-      {
-        type: 'confirm',
-        name: 'someAnswer',
-        message: 'Would you like to enable this option?',
-        default: true
-      }
+      { name: 'botName', message: `What 's the name of your bot?`, default: 'sample' },
+      { name: 'description', message: 'What will your bot do?', default: 'sample' },
+      { name: 'dialog', type: 'list', message: 'What default dialog do you want?', choices: ['Echo'] },
     ];
 
     return this.prompt(prompts).then(props => {
@@ -26,13 +27,23 @@ module.exports = class extends Generator {
   }
 
   writing() {
-    this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
-    );
+    const directoryName = _.kebabCase(this.props.botName);
+    const defaultDialog = this.props.dialog.split(' ')[0].toLowerCase();
+
+    if (path.basename(this.destinationPath()) !== directoryName) {
+      this.log(`Your bot should be in a directory named ${directoryName}\nI'll automatically create this folder.`);
+      mkdirp(directoryName);
+      this.destinationRoot(this.destinationPath(directoryName));
+    }
+
+    this.fs.copyTpl(this.templatePath('pom.xml'), this.destinationPath('pom.xml'), { botName: directoryName });
+    this.fs.copy(this.templatePath(`app.java`), this.destinationPath(`app.java`));
+    this.fs.copyTpl(this.templatePath('README.md'), this.destinationPath('README.md'), {
+      botName: this.props.botName, description: this.props.description
+    });
   }
 
   install() {
-    this.installDependencies();
+    this.installDependencies({bower: false});
   }
 };
---
+++ Generator/generator-botbuilder-java/generators/app/templates/app.java
@@ -0,0 +1,112 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.microsoft.bot.connector.sample;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.microsoft.aad.adal4j.AuthenticationException;
+import com.microsoft.bot.connector.customizations.CredentialProvider;
+import com.microsoft.bot.connector.customizations.CredentialProviderImpl;
+import com.microsoft.bot.connector.customizations.JwtTokenValidation;
+import com.microsoft.bot.connector.customizations.MicrosoftAppCredentials;
+import com.microsoft.bot.connector.implementation.ConnectorClientImpl;
+import com.microsoft.bot.schema.models.Activity;
+import com.microsoft.bot.schema.models.ActivityTypes;
+import com.microsoft.bot.schema.models.ResourceResponse;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.InetSocketAddress;
+import java.net.URLDecoder;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class App {
+    private static final Logger LOGGER = Logger.getLogger( App.class.getName() );
+    private static String appId = "";       // <-- app id -->
+    private static String appPassword = ""; // <-- app password -->
+
+    public static void main( String[] args ) throws IOException {
+        CredentialProvider credentialProvider = new CredentialProviderImpl(appId, appPassword);
+        HttpServer server = HttpServer.create(new InetSocketAddress(3978), 0);
+        server.createContext("/api/messages", new MessageHandle(credentialProvider));
+        server.setExecutor(null);
+        server.start();
+    }
+
+    static class MessageHandle implements HttpHandler {
+        private ObjectMapper objectMapper;
+        private CredentialProvider credentialProvider;
+        private MicrosoftAppCredentials credentials;
+
+        MessageHandle(CredentialProvider credentialProvider) {
+            this.objectMapper = new ObjectMapper()
+                    .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+                    .findAndRegisterModules();
+            this.credentialProvider = credentialProvider;
+            this.credentials = new MicrosoftAppCredentials(appId, appPassword);
+        }
+
+        public void handle(HttpExchange httpExchange) throws IOException {
+            if (httpExchange.getRequestMethod().equalsIgnoreCase("POST")) {
+                Activity activity = getActivity(httpExchange);
+                String authHeader = httpExchange.getRequestHeaders().getFirst("Authorization");
+                try {
+                    JwtTokenValidation.assertValidActivity(activity, authHeader, credentialProvider);
+
+                    // send ack to user activity
+                    httpExchange.sendResponseHeaders(202, 0);
+                    httpExchange.getResponseBody().close();
+
+                    if (activity.type().equals(ActivityTypes.MESSAGE)) {
+                        // reply activity with the same text
+                        ConnectorClientImpl connector = new ConnectorClientImpl(activity.serviceUrl(), this.credentials);
+                        ResourceResponse response = connector.conversations().sendToConversation(activity.conversation().id(),
+                                new Activity()
+                                        .withType(ActivityTypes.MESSAGE)
+                                        .withText("Echo: " + activity.text())
+                                        .withRecipient(activity.from())
+                                        .withFrom(activity.recipient())
+                        );
+                    }
+                } catch (AuthenticationException ex) {
+                    httpExchange.sendResponseHeaders(401, 0);
+                    httpExchange.getResponseBody().close();
+                    LOGGER.log(Level.WARNING, "Auth failed!", ex);
+                } catch (Exception ex) {
+                    LOGGER.log(Level.WARNING, "Execution failed", ex);
+                }
+            }
+        }
+
+        private String getRequestBody(HttpExchange httpExchange) throws IOException {
+            StringBuilder buffer = new StringBuilder();
+            InputStream stream = httpExchange.getRequestBody();
+            int rByte;
+            while ((rByte = stream.read()) != -1) {
+                buffer.append((char)rByte);
+            }
+            stream.close();
+            if (buffer.length() > 0) {
+                return URLDecoder.decode(buffer.toString(), "UTF-8");
+            }
+            return "";
+        }
+
+        private Activity getActivity(HttpExchange httpExchange) {
+            try {
+                String body = getRequestBody(httpExchange);
+                LOGGER.log(Level.INFO, body);
+                return objectMapper.readValue(body, Activity.class);
+            } catch (Exception ex) {
+                LOGGER.log(Level.WARNING, "Failed to get activity", ex);
+                return null;
+            }
+
+        }
+    }
+}
