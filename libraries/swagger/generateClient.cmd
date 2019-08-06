f046a8f2e352f192a383f15e44b1da3efcf20ba0
Tracy Boehrer <tracy.boehrer@gmail.com>
2019-08-06 09:52:58 -0700

    Corrections to autorest generate command

--- libraries/swagger/generateClient.cmd
+++ libraries/swagger/generateClient.cmd
@@ -1,11 +1,11 @@
 call npm install replace@0.3.0
 
-
+del /q generated
 del /q ..\botbuilder-schema\src\main\java\com\microsoft\bot\schema\models\
 
-call autorest .\README.md --java --add-credentials true
+call autorest .\README.md --java --add-credentials
 
-robocopy .\generated\models ..\botbuilder-schema\src\main\java\com\microsoft\bot\schema\models *.* /move /xf *Exception.java
+robocopy .\generated\src\main\java\com\microsoft\bot\schema\models ..\botbuilder-schema\src\main\java\com\microsoft\bot\schema\models *.* /move /xf *Exception.java
 
 call .\node_modules\.bin\replace "import com.microsoft.bot.schema.models.ErrorResponseException;" "import com.microsoft.bot.connector.models.ErrorResponseException;" . -r -q --include="*.java"
 call .\node_modules\.bin\replace "import com.microsoft.bot.schema.ConnectorClient;" "import com.microsoft.bot.connector.ConnectorClient;" . -r -q --include="*.java"
@@ -14,4 +14,4 @@ call .\node_modules\.bin\replace "import com.microsoft.bot.schema.Conversations;
 call .\node_modules\.bin\replace "import com.microsoft.rest.RestException;" "import com.microsoft.rest.RestException;import com.microsoft.bot.schema.models.ErrorResponse;" . -r -q --include="ErrorResponseException.java"
 call .\node_modules\.bin\replace "package com.microsoft.bot.schema" "package com.microsoft.bot.connector" . -r -q --include="*.java"
 
-robocopy .\generated ..\bot-connector\src\main\java\com\microsoft\bot\connector *.* /e /move
+robocopy .\generated\src\main\java\com\microsoft\bot\schema ..\bot-connector\src\main\java\com\microsoft\bot\connector *.* /e /move
