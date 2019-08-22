f3d41517cd6791b8d06121cad29d7f17833d6b56
Tracy Boehrer <tracy.boehrer@gmail.com>
2019-08-22 09:03:29 -0500

    Moved EmulatorValidation TokenValidationParameters.

--- libraries/bot-connector/src/main/java/com/microsoft/bot/connector/authentication/EmulatorValidation.java
+++ libraries/bot-connector/src/main/java/com/microsoft/bot/connector/authentication/EmulatorValidation.java
@@ -8,6 +8,8 @@ import com.auth0.jwt.interfaces.DecodedJWT;
 import com.microsoft.aad.adal4j.AuthenticationException;
 import org.apache.commons.lang3.StringUtils;
 
+import java.time.Duration;
+import java.util.ArrayList;
 import java.util.concurrent.CompletableFuture;
 
 /**
@@ -17,7 +19,21 @@ public class EmulatorValidation {
     /**
      * TO BOT FROM EMULATOR: Token validation parameters when connecting to a channel.
      */
-    private static final TokenValidationParameters TOKENVALIDATIONPARAMETERS = TokenValidationParameters.toBotFromEmulatorTokenValidationParameters();
+    private static final TokenValidationParameters TOKENVALIDATIONPARAMETERS = new TokenValidationParameters() {{
+            this.validateIssuer = true;
+            this.validIssuers = new ArrayList<String>() {{
+                add("https://sts.windows.net/d6d49420-f39b-4df7-a1dc-d59a935871db/");               // Auth v3.1, 1.0 token
+                add("https://login.microsoftonline.com/d6d49420-f39b-4df7-a1dc-d59a935871db/v2.0"); // Auth v3.1, 2.0 token
+                add("https://sts.windows.net/f8cdef31-a31e-4b4a-93e4-5f571e91255a/");               // Auth v3.2, 1.0 token
+                add("https://login.microsoftonline.com/f8cdef31-a31e-4b4a-93e4-5f571e91255a/v2.0"); // Auth v3.2, 2.0 token
+                add("https://sts.windows.net/cab8a31a-1906-4287-a0d8-4eef66b95f6e/");               // Auth for US Gov, 1.0 token
+                add("https://login.microsoftonline.us/cab8a31a-1906-4287-a0d8-4eef66b95f6e/v2.0");  // Auth for US Gov, 2.0 token
+            }};
+            this.validateAudience = false;
+            this.validateLifetime = true;
+            this.clockSkew = Duration.ofMinutes(5);
+            this.requireSignedTokens = true;
+        }};
 
     /**
      * Determines if a given Auth header is from the Bot Framework Emulator
