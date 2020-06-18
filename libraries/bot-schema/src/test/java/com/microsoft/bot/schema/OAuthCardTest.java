c806cd134ee5c14df21c81758acf4efdec4665db
Santiago Grangetto <santiagograngetto@gmail.com>
2020-06-18 14:53:34 -0300

    Add test to OAuthCard toAttachment method

---
+++ libraries/bot-schema/src/test/java/com/microsoft/bot/schema/OAuthCardTest.java
@@ -0,0 +1,23 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.microsoft.bot.schema;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class OAuthCardTest {
+    OAuthCard card = new OAuthCard(){
+        {
+            setText("Test OAuth Text");
+            setConnectionName("Test Connection Name");
+        }
+    };
+
+    @Test
+    public void testToAttachment() {
+        Attachment attachment = card.toAttachment();
+        Assert.assertNotNull(attachment);
+        Assert.assertEquals("application/vnd.microsoft.card.oauth", attachment.getContentType());
+    }
+}
