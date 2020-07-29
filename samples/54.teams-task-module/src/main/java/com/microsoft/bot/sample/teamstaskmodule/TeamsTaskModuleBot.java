6ef7451ca95b2a2abf54e59bb92cbcb4565129db
tracyboehrer <tracyboehrer@users.noreply.github.com>
2020-07-29 15:29:38 -0500

    Initial iFrame based Teams Task Module sample

--- samples/54.teams-task-module/src/main/java/com/microsoft/bot/sample/teamstaskmodule/TeamsTaskModuleBot.java
+++ samples/54.teams-task-module/src/main/java/com/microsoft/bot/sample/teamstaskmodule/TeamsTaskModuleBot.java
@@ -3,19 +3,31 @@
 
 package com.microsoft.bot.sample.teamstaskmodule;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.microsoft.bot.builder.MessageFactory;
 import com.microsoft.bot.builder.TurnContext;
 import com.microsoft.bot.builder.teams.TeamsActivityHandler;
-import com.microsoft.bot.schema.Activity;
+import com.microsoft.bot.integration.Async;
+import com.microsoft.bot.integration.Configuration;
+import com.microsoft.bot.sample.teamstaskmodule.models.AdaptiveCardTaskFetchValue;
+import com.microsoft.bot.sample.teamstaskmodule.models.CardTaskFetchValue;
+import com.microsoft.bot.sample.teamstaskmodule.models.TaskModuleIds;
+import com.microsoft.bot.sample.teamstaskmodule.models.TaskModuleResponseFactory;
+import com.microsoft.bot.sample.teamstaskmodule.models.TaskModuleUIConstants;
+import com.microsoft.bot.sample.teamstaskmodule.models.UISettings;
 import com.microsoft.bot.schema.Attachment;
+import com.microsoft.bot.schema.CardAction;
 import com.microsoft.bot.schema.HeroCard;
+import com.microsoft.bot.schema.Serialization;
 import com.microsoft.bot.schema.teams.*;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.CompletionException;
+import java.util.stream.Collectors;
 import org.apache.commons.io.IOUtils;
-import org.json.JSONObject;
 import org.springframework.stereotype.Component;
 
 import java.io.InputStream;
@@ -34,23 +46,29 @@ import java.util.concurrent.CompletableFuture;
  */
 @Component
 public class TeamsTaskModuleBot extends TeamsActivityHandler {
+    private final String baseUrl;
+    private final List<UISettings> actions = Arrays.asList(
+        TaskModuleUIConstants.ADAPTIVECARD,
+        TaskModuleUIConstants.CUSTOMFORM,
+        TaskModuleUIConstants.YOUTUBE
+    );
 
-    @Override
-    protected CompletableFuture<Void> onTeamsMembersAdded(
-        List<TeamsChannelAccount> membersAdded,
-        TeamInfo teamInfo,
-        TurnContext turnContext
-    ) {
-        return turnContext.sendActivity(MessageFactory.attachment(getTaskModuleHeroCard()))
-            .thenApply(resourceResponse -> null);
+    public TeamsTaskModuleBot(Configuration configuration) {
+        baseUrl = configuration.getProperty("BaseUrl");
     }
 
     @Override
     protected CompletableFuture<Void> onMessageActivity(
         TurnContext turnContext
     ) {
-        Attachment attachment = getTaskModuleHeroCard();
-        return turnContext.sendActivity(MessageFactory.attachment(attachment))
+        // This displays two cards: A HeroCard and an AdaptiveCard.  Both have the same
+        // options.  When any of the options are selected, `onTeamsTaskModuleFetch`
+        // is called.
+        return turnContext.sendActivity(MessageFactory.attachment(Arrays.asList(
+                getTaskModuleHeroCardOptions(),
+                getTaskModuleAdaptiveCardOptions()
+            ))
+        )
             .thenApply(resourceResponse -> null);
     }
 
@@ -59,33 +77,47 @@ public class TeamsTaskModuleBot extends TeamsActivityHandler {
         TurnContext turnContext,
         TaskModuleRequest taskModuleRequest
     ) {
-        Activity reply;
-        try {
-            reply = MessageFactory.text(
-                "onTeamsTaskModuleFetch TaskModuleRequest: " +
-                    new ObjectMapper().writeValueAsString(taskModuleRequest));
-        } catch (JsonProcessingException e) {
-            CompletableFuture<TaskModuleResponse> result = new CompletableFuture<>();
-            result.completeExceptionally(new CompletionException(e));
-            return result;
-        }
+        // Called when the user selects an options from the displayed HeroCard or
+        // AdaptiveCard.  The result is the action to perform.
+        return Async.tryCompletion(() -> {
+            CardTaskFetchValue<String> value = Serialization
+                .safeGetAs(taskModuleRequest.getData(), CardTaskFetchValue.class);
 
-        return turnContext.sendActivity(reply)
-            .thenApply(resourceResponse -> {
-                Attachment adaptiveCard = getTaskModuleAdaptiveCard();
+            TaskModuleTaskInfo taskInfo = new TaskModuleTaskInfo();
+            switch (value.getData()) {
+                // Display the YouTube.html page
+                case TaskModuleIds.YOUTUBE: {
+                    String url = baseUrl + "/" + TaskModuleIds.YOUTUBE;
+                    taskInfo.setUrl(url);
+                    taskInfo.setFallbackUrl(url);
+                    setTaskInfo(taskInfo, TaskModuleUIConstants.YOUTUBE);
+                    break;
+                }
 
-                return new TaskModuleResponse() {{
-                    setTask(new TaskModuleContinueResponse() {{
-                        setType("continue");
-                        setValue(new TaskModuleTaskInfo() {{
-                            setCard(adaptiveCard);
-                            setHeight(200);
-                            setWidth(400);
-                            setTitle("Adaptive Card: Inputs");
-                        }});
-                    }});
-                }};
-            });
+                // Display the CustomForm.html page, and post the form data back via
+                // onTeamsTaskModuleSubmit.
+                case TaskModuleIds.CUSTOMFORM: {
+                    String url = baseUrl + "/" + TaskModuleIds.CUSTOMFORM;
+                    taskInfo.setUrl(url);
+                    taskInfo.setFallbackUrl(url);
+                    setTaskInfo(taskInfo, TaskModuleUIConstants.CUSTOMFORM);
+                    break;
+                }
+
+                // Display an AdaptiveCard to prompt user for text, and post it back via
+                // onTeamsTaskModuleSubmit.
+                case TaskModuleIds.ADAPTIVECARD:
+                    taskInfo.setCard(createAdaptiveCardAttachment());
+                    setTaskInfo(taskInfo, TaskModuleUIConstants.ADAPTIVECARD);
+                    break;
+
+                default:
+                    break;
+            }
+
+            return taskInfo;
+        })
+            .thenApply(TaskModuleResponseFactory::toTaskModuleResponse);
     }
 
     @Override
@@ -93,58 +125,79 @@ public class TeamsTaskModuleBot extends TeamsActivityHandler {
         TurnContext turnContext,
         TaskModuleRequest taskModuleRequest
     ) {
-        Activity reply;
-        try {
-            reply = MessageFactory.text(
-                "onTeamsTaskModuleSubmit TaskModuleRequest: " +
-                    new ObjectMapper().writeValueAsString(taskModuleRequest));
-        } catch (JsonProcessingException e) {
-            CompletableFuture<TaskModuleResponse> result = new CompletableFuture<>();
-            result.completeExceptionally(new CompletionException(e));
-            return result;
-        }
-
-        turnContext.sendActivity(reply)
-            .thenApply(resourceResponse -> null);
-
-        return CompletableFuture.completedFuture(new TaskModuleResponse() {{
-            setTask(new TaskModuleMessageResponse() {{
-                setType("message");
-                setValue("Thanks!");
-            }});
-        }});
+        // Called when data is being returned from the selected option (see `onTeamsTaskModuleFetch').
+        return Async.tryCompletion(() ->
+            // Echo the users input back.  In a production bot, this is where you'd add behavior in
+            // response to the input.
+            MessageFactory.text(
+                "onTeamsTaskModuleSubmit TaskModuleRequest: "
+                    + new ObjectMapper().writeValueAsString(taskModuleRequest)
+            )
+        )
+            .thenCompose(turnContext::sendActivity)
+            .thenApply(resourceResponse -> TaskModuleResponseFactory.createResponse("Thanks!"));
     }
 
-    private Attachment getTaskModuleHeroCard() {
+    private void setTaskInfo(TaskModuleTaskInfo taskInfo, UISettings uiSettings) {
+        taskInfo.setHeight(uiSettings.getHeight());
+        taskInfo.setWidth(uiSettings.getWidth());
+        taskInfo.setTitle(uiSettings.getTitle());
+    }
+
+    private Attachment getTaskModuleHeroCardOptions() {
+        List<CardAction> buttons = actions.stream().map(cardType ->
+            new TaskModuleAction(cardType.getButtonTitle(),
+                new CardTaskFetchValue<String>() {{
+                    setData(cardType.getId());
+                }})
+        ).collect(Collectors.toCollection(ArrayList::new));
+
         HeroCard card = new HeroCard() {{
             setTitle("Task Module Invocation from Hero Card");
-            setSubtitle(
-                "This is a hero card with a Task Module Action button.  Click the button to show an Adaptive Card within a Task Module.");
-            setButtons(Arrays.asList(
-                new TaskModuleAction(
-                    "Adaptive Card",
-                    new JSONObject().put(
-                        "data",
-                        "adaptivecard"
-                    ).toString()
-                )
-            ));
+            setButtons(buttons);
         }};
         return card.toAttachment();
     }
 
-    private Attachment getTaskModuleAdaptiveCard() {
-        try {
-            InputStream inputStream = getClass().getClassLoader()
-                .getResourceAsStream("adaptivecard.json");
-            String result = IOUtils.toString(inputStream, StandardCharsets.UTF_8);
+    private Attachment getTaskModuleAdaptiveCardOptions() {
+        try (InputStream inputStream = getClass().getClassLoader()
+            .getResourceAsStream("adaptiveTemplate.json")
+        ) {
+            String cardTemplate = IOUtils.toString(inputStream, StandardCharsets.UTF_8);
 
-            return new Attachment() {{
-                setContentType("application/vnd.microsoft.card.adaptive");
-                setContent(new ObjectMapper().readValue(result, ObjectNode.class));
-            }};
+            List<Map<String,Object>> cardActions = actions.stream().map(cardType -> {
+                Map<String, Object> a = new HashMap<>();
+                a.put("type", "Action.Submit");
+                a.put("title", cardType.getButtonTitle());
+                a.put("data", new AdaptiveCardTaskFetchValue<String>() {{
+                    setData(cardType.getId());
+                }});
+                return a;
+            }).collect(Collectors.toCollection(ArrayList::new));
+
+            String adaptiveCardJson = String.format(cardTemplate, Serialization.toString(cardActions));
+
+            return adaptiveCardAttachmentFromJson(adaptiveCardJson);
         } catch (Throwable t) {
             throw new CompletionException(t);
         }
+    }
+
+    private Attachment createAdaptiveCardAttachment() {
+        try (InputStream inputStream = getClass().getClassLoader()
+            .getResourceAsStream("adaptivecard.json")
+        ) {
+            String result = IOUtils.toString(inputStream, StandardCharsets.UTF_8);
+            return adaptiveCardAttachmentFromJson(result);
+        } catch (Throwable t) {
+            throw new CompletionException(t);
+        }
+    }
+
+    private Attachment adaptiveCardAttachmentFromJson(String json) throws IOException {
+        return new Attachment() {{
+            setContentType("application/vnd.microsoft.card.adaptive");
+            setContent(new ObjectMapper().readValue(json, ObjectNode.class));
+        }};
     }
 }
