d7dcd293ec5083ab18eae5f5f563e3a9ef234efc
Lee Parrish <lparrish@microsoft.com>
2020-10-27 16:56:15 -0500

    Fix Checkstyle issues

--- libraries/bot-schema/src/main/java/com/microsoft/bot/schema/SpeechConstants.java
+++ libraries/bot-schema/src/main/java/com/microsoft/bot/schema/SpeechConstants.java
@@ -6,7 +6,7 @@ package com.microsoft.bot.schema;
 /**
  * Defines constants that can be used in the processing of speech interactions.
  */
-public class SpeechConstants {
+public final class SpeechConstants {
 
     private SpeechConstants() {
 
@@ -16,5 +16,6 @@ public class SpeechConstants {
      * The xml tag structure to indicate an empty speak tag, to be used in the 'speak' property of an Activity.
      * When this is set it indicates to the channel that speech should not be generated.
      */
-    public static final String EMPTY_SPEAK_TAG = "<speak version=\"1.0\" xmlns=\"https://www.w3.org/2001/10/synthesis\" xml:lang=\"en-US\" />";
+    public static final String EMPTY_SPEAK_TAG =
+        "<speak version=\"1.0\" xmlns=\"https://www.w3.org/2001/10/synthesis\" xml:lang=\"en-US\" />";
 }
