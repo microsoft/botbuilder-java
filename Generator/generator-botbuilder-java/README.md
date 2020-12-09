6dda8efa84f15171b69a2d4b793d2dac50411fc6
tracyboehrer <tracyboehrer@users.noreply.github.com>
2020-12-09 08:32:02 -0600

    Added Empty template for generator.  Updated README

--- Generator/generator-botbuilder-java/README.md
+++ Generator/generator-botbuilder-java/README.md
@@ -1,29 +1,120 @@
-# generator-botbuilder-java [![NPM version][npm-image]][npm-url] [![Build Status][travis-image]][travis-url] [![Dependency Status][daviddm-image]][daviddm-url] [![Coverage percentage][coveralls-image]][coveralls-url]
-> Template to create conversational bots in Java using Microsoft Bot Framework.
+# generator-botbuilder-java
+
+Yeoman generator for [Bot Framework v4](https://dev.botframework.com).  Will let you quickly set up a conversational AI bot
+using core AI capabilities.
+
+## About
+
+`generator-botbuilder-java` will help you build new conversational AI bots using the [Bot Framework v4](https://dev.botframework.com).
+
+## Templates
+
+The generator supports three different template options.  The table below can help guide which template is right for you.
+
+|  Template  |  Description  |
+| ---------- |  ---------  |
+| Echo&nbsp;Bot | A good template if you want a little more than "Hello World!", but not much more.  This template handles the very basics of sending messages to a bot, and having the bot process the messages by repeating them back to the user.  This template produces a bot that simply "echoes" back to the user anything the user says to the bot. |
+| Empty&nbsp;Bot | A good template if you are familiar with Bot Framework v4, and simply want a basic skeleton project.  Also a good option if you want to take sample code from the documentation and paste it into a minimal bot in order to learn. |
+
+### How to Choose a Template
+
+| Template | When This Template is a Good Choice |
+| -------- | -------- |
+| Echo&nbsp;Bot  | You are new to Bot Framework v4 and want a working bot with minimal features. |
+| Empty&nbsp;Bot  | You are a seasoned Bot Framework v4 developer.  You've built bots before, and want the minimum skeleton of a bot. |
+
+### Template Overview
+
+#### Echo Bot Template
+
+The Echo Bot template is slightly more than the a classic "Hello World!" example, but not by much.  This template shows the basic structure of a bot, how a bot recieves messages from a user, and how a bot sends messages to a user.  The bot will "echo" back to the user, what the user says to the bot.  It is a good choice for first time, new to Bot Framework v4 developers.
+
+#### Empty Bot Template
+
+The Empty Bot template is the minimal skeleton code for a bot.  It provides a stub `onTurn` handler but does not perform any actions.  If you are experienced writing bots with Bot Framework v4 and want the minimum scaffolding, the Empty template is for you.
 
 ## Installation
 
-First, install [Yeoman](http://yeoman.io) and generator-botbuilder-java using [npm](https://www.npmjs.com/) (we assume you have pre-installed [node.js](https://nodejs.org/)).
+1. Install [Yeoman](http://yeoman.io) using [npm](https://www.npmjs.com) (we assume you have pre-installed [node.js](https://nodejs.org/)).
+
+    ```bash
+    # Make sure both are installed globally
+    npm install -g yo
+    ```
+
+2. Install generator-botbuilder-java by typing the following in your console:
+
+    ```bash
+    # Make sure both are installed globally
+    npm install -g generator-botbuilder-java
+    ```
+
+3. Verify that Yeoman and generator-botbuilder-java have been installed correctly by typing the following into your console:
+
+    ```bash
+    yo botbuilder-java --help
+    ```
+
+## Usage
+
+### Creating a New Bot Project
+
+When the generator is launched, it will prompt for the information required to create a new bot.
 
 ```bash
-npm install -g yo
-npm install -g generator-botbuilder-java
-```
-
-Then generate your new project:
-
-```bash
+# Run the generator in interactive mode
 yo botbuilder-java
 ```
 
-## License
+### Generator Command Line Options
 
-MIT © [Microsoft]()
+The generator supports a number of command line options that can be used to change the generator's default options or to pre-seed a prompt.
 
+| Command&nbsp;line&nbsp;Option  | Description |
+| ------------------- | ----------- |
+| --help, -h        | List help text for all supported command-line options |
+| --botName, -N     | The name given to the bot project |
+| --packageName, -P | The Java package name to use for the bot |
+| --template, -T    | The template used to generate the project.  Options are `empty`, or `echo`.  See [https://aka.ms/botbuilder-generator](https://aka.ms/botbuilder-generator) for additional information regarding the different template option and their functional differences. |
+| --noprompt        | The generator will not prompt for confirmation before creating a new bot.  Any requirement options not passed on the command line will use a reasonable default value.  This option is intended to enable automated bot generation for testing purposes. |
 
-[npm-image]: https://badge.fury.io/js/generator-botbuilder-java.svg
-[npm-url]: https://npmjs.org/package/generator-botbuilder-java
-[travis-image]: https://travis-ci.org/Microsoft/generator-botbuilder-java.svg?branch=master
-[travis-url]: https://travis-ci.org/Microsoft/generator-botbuilder-java
-[daviddm-image]: https://david-dm.org/Microsoft/generator-botbuilder-java.svg?theme=shields.io
-[daviddm-url]: https://david-dm.org/Microsoft/generator-botbuilder-java
+#### Example Using Command Line Options
+
+This example shows how to pass command line options to the generator, setting the default language to TypeScript and the default template to Core.
+
+```bash
+# Run the generator defaulting the pacakge name and the template
+yo botbuilder-java --P "com.mycompany.bot" --T "echo"
+```
+
+### Generating a Bot Using --noprompt
+
+The generator can be run in `--noprompt` mode, which can be used for automated bot creation.  When run in `--noprompt` mode, the generator can be configured using command line options as documented above.  If a command line option is ommitted a reasonable default will be used.  In addition, passing the `--noprompt` option will cause the generator to create a new bot project without prompting for confirmation before generating the bot.
+
+#### Default Options
+
+| Command&nbsp;line&nbsp;Option  | Default Value |
+| ------------------- | ----------- |
+| --botname, -N     | `echo` |
+| --packageName, -p | `echo` |
+| --template, -T    | `echo` |
+
+#### Examples Using --noprompt
+
+This example shows how to run the generator in --noprompt mode, setting all required options on the command line.
+
+```bash
+# Run the generator, setting all command line options
+yo botbuilder-java --noprompt -N "MyEchoBot" -P "com.mycompany.bot.echo" -T "echo"
+```
+
+This example shows how to run the generator in --noprompt mode, using all the default command line options.  The generator will create a bot project using all the default values specified in the **Default Options** table above.
+
+```bash
+# Run the generator using all default options
+yo botbuilder-java --noprompt
+```
+
+## Logging Issues and Providing Feedback
+
+Issues and feedback about the botbuilder generator can be submitted through the project's [GitHub Issues](https://github.com/Microsoft/botbuilder-samples/issues) page.
--- Generator/generator-botbuilder-java/generators/app/index.js
+++ Generator/generator-botbuilder-java/generators/app/index.js
@@ -1,77 +1,268 @@
 'use strict';
+const pkg = require('../../package.json');
 const Generator = require('yeoman-generator');
 const path = require('path');
 const _ = require('lodash');
+const chalk = require('chalk');
 const mkdirp = require('mkdirp');
 
+const BOT_TEMPLATE_NAME_EMPTY = 'Empty Bot';
+const BOT_TEMPLATE_NAME_SIMPLE = 'Echo Bot';
+const BOT_TEMPLATE_NAME_CORE = 'Core Bot';
+
+const BOT_TEMPLATE_NOPROMPT_EMPTY = 'empty';
+const BOT_TEMPLATE_NOPROMPT_SIMPLE = 'echo';
+const BOT_TEMPLATE_NOPROMPT_CORE = 'core';
+
 module.exports = class extends Generator {
-  prompting() {
-    const prompts = [
-      {
-        name: 'botName',
-        message: `What's the name of your bot?`,
-        type: String,
-        default: 'echo'
-      },
-      {
-        name: 'packageName',
-        message: `What's the fully qualified package name of your bot?`,
-        type: String,
-        default: 'echo'
-      },
-      {
-        name: 'template',
-        type: 'list',
-        message: 'What default template do you want?',
-        choices: ['echo']
-      }
-    ];
+    constructor(args, opts) {
+        super(args, opts);
 
-    return this.prompt(prompts).then(props => {
-      // To access props later use this.props.someAnswer;
-      this.props = props;
-    });
-  }
+        // allocate an object that we can use to store our user prompt values from our askFor* functions
+        this.templateConfig = Object.create(null);
 
-  writing() {
-    const botName = this.props.botName;
-    const packageName = this.props.packageName.toLowerCase();
-    const packageTree = packageName.replace(/\./g, '/');
-    const directoryName = _.kebabCase(this.props.botName);
-    const defaultDialog = this.props.template.split(' ')[0].toLowerCase();
-
-    if (path.basename(this.destinationPath()) !== directoryName) {
-      this.log(`Your bot should be in a directory named ${directoryName}`);
-      mkdirp(directoryName);
-      this.destinationRoot(this.destinationPath(directoryName));
+        // configure the commandline options
+        this._configureCommandlineOptions(this);
     }
 
-    // Copy the project tree
-    this.fs.copyTpl(
-      this.templatePath(path.join(defaultDialog, 'tree', '**')),
-      this.destinationPath(),
-      {
-        botName,
-        packageName
-      }
-    );
+    initializing() {
+        // give the user some data before we start asking them questions
+        this.log(`\nWelcome to the Microsoft Java Bot Builder generator v${pkg.version}. `);
+    }
 
-    // Copy main source
-    this.fs.copyTpl(
-      this.templatePath(path.join(defaultDialog, 'src/main/java/**')),
-      this.destinationPath(path.join('src/main/java', packageTree)),
-      {
-        packageName
-      }
-    );
+    prompting() {
+        const userPrompts = this._getPrompts();
+        let result = Promise.resolve();
 
-    // Copy test source
-    this.fs.copyTpl(
-      this.templatePath(path.join(defaultDialog, 'src/test/java/**')),
-      this.destinationPath(path.join('src/test/java', packageTree)),
-      {
-        packageName
-      }
-    );
-  }
+        // if we're told to not prompt, then pick what we need and return
+        if(this.options.noprompt) {
+            // this function will throw if it encounters errors/invalid options
+            this._verifyNoPromptOptions();
+        }
+
+        // run all prompts in sequence.  Results can be ignored.
+        for(let taskName in userPrompts) {
+            let prompt = userPrompts[taskName];
+            result = result.then(_ => {
+                return new Promise((s, r) => {
+                    setTimeout(_ => prompt().then(s, r), 0);    // set timeout is required, otherwise node hangs
+                });
+            })
+        }
+        return result;
+    }
+
+    writing() {
+        // if the user confirmed their settings, then lets go ahead
+        // an install module dependencies
+        if(this.templateConfig.finalConfirmation === true) {
+            const botName = this.templateConfig.botName;
+            const packageName = this.templateConfig.packageName.toLowerCase();
+            const packageTree = packageName.replace(/\./g, '/');
+            const directoryName = _.kebabCase(this.templateConfig.botName);
+            const template = this.templateConfig.template.split(' ')[0].toLowerCase();
+
+            if (path.basename(this.destinationPath()) !== directoryName) {
+                mkdirp(directoryName);
+                this.destinationRoot(this.destinationPath(directoryName));
+            }
+
+            // Copy the project tree
+            this.fs.copyTpl(
+                this.templatePath(path.join(template, 'tree', '**')),
+                this.destinationPath(),
+                {
+                    botName,
+                    packageName
+                }
+            );
+
+            // Copy main source
+            this.fs.copyTpl(
+                this.templatePath(path.join(template, 'src/main/java/**')),
+                this.destinationPath(path.join('src/main/java', packageTree)),
+                {
+                    packageName
+                }
+            );
+
+            // Copy test source
+            this.fs.copyTpl(
+                this.templatePath(path.join(template, 'src/test/java/**')),
+                this.destinationPath(path.join('src/test/java', packageTree)),
+                {
+                    packageName
+                }
+            );
+        }
+    }
+
+    end() {
+        if(this.templateConfig.finalConfirmation === true) {
+            this.log(chalk.green('------------------------ '));
+            this.log(chalk.green(' Your new bot is ready!  '));
+            this.log(chalk.green('------------------------ '));
+            this.log(`Your bot should be in a directory named ${_.kebabCase(this.templateConfig.botName)}`);
+            this.log('Open the ' + chalk.green.bold('README.md') + ' to learn how to run your bot. ');
+            this.log('Thank you for using the Microsoft Bot Framework. ');
+            this.log('\n< ** > The Bot Framework Team');
+        } else {
+            this.log(chalk.red.bold('-------------------------------- '));
+            this.log(chalk.red.bold(' New bot creation was canceled. '));
+            this.log(chalk.red.bold('-------------------------------- '));
+            this.log('Thank you for using the Microsoft Bot Framework. ');
+            this.log('\n< ** > The Bot Framework Team');
+        }
+    }
+
+    _configureCommandlineOptions() {
+        this.option('botName', {
+            desc: 'The name you want to give to your bot',
+            type: String,
+            default: 'echo',
+            alias: 'N'
+        });
+
+        this.option('packageName', {
+            desc: `What's the fully qualified package name of your bot?`,
+            type: String,
+            default: 'com.mycompany.echo',
+            alias: 'P'
+        });
+
+        const templateDesc = `The initial bot capabilities. (${BOT_TEMPLATE_NAME_EMPTY} | ${BOT_TEMPLATE_NAME_SIMPLE} | ${BOT_TEMPLATE_NAME_CORE})`;
+        this.option('template', {
+            desc: templateDesc,
+            type: String,
+            default: BOT_TEMPLATE_NAME_SIMPLE,
+            alias: 'T'
+        });
+
+        this.argument('noprompt', {
+            desc: 'Do not prompt for any information or confirmation',
+            type: Boolean,
+            required: false,
+            default: false
+        });
+    }
+
+    _getPrompts() {
+        const noprompt = this.options.noprompt;
+        const prompts = {
+            // ask the user to name their bot
+            askForBotName: () => {
+                if(noprompt) {
+                    return Promise.resolve();
+                }
+
+                return this.prompt({
+                    type: 'input',
+                    name: 'botName',
+                    message: `What's the name of your bot?`,
+                    default: (this.options.botName ? this.options.botName : 'echo')
+                }).then(answer => {
+                    // store the botname description answer
+                    this.templateConfig.botName = answer.botName;
+                });
+            },
+
+            // ask for package name
+            askForPackageName: () => {
+                if(noprompt) {
+                    return Promise.resolve();
+                }
+
+                return this.prompt({
+                    type: 'input',
+                    name: 'packageName',
+                    message: `What's the fully qualified package name of your bot?`,
+                    default: (this.options.packageName ? this.options.packageName : 'com.mycompany.echo')
+                }).then(answer => {
+                    // store the package name description answer
+                    this.templateConfig.packageName = answer.packageName;
+                });
+            },
+
+
+            // ask the user which bot template we should use
+            askForBotTemplate: () => {
+                if(noprompt) {
+                    return Promise.resolve();
+                }
+
+                return this.prompt({
+                    type: 'list',
+                    name: 'template',
+                    message: 'Which template would you like to start with?',
+                    choices: [
+                        {
+                            name: BOT_TEMPLATE_NAME_SIMPLE,
+                            value: BOT_TEMPLATE_NOPROMPT_SIMPLE
+                        },
+                        {
+                            name: BOT_TEMPLATE_NAME_EMPTY,
+                            value: BOT_TEMPLATE_NOPROMPT_EMPTY
+                        }
+
+                        /*,
+                        {
+                            name: BOT_TEMPLATE_NAME_CORE,
+                            value: BOT_TEMPLATE_NOPROMPT_CORE
+                        }
+                       */
+                    ],
+                    default: (this.options.template ? _.toLower(this.options.template) : BOT_TEMPLATE_NOPROMPT_SIMPLE)
+                    }).then(answer => {
+                        // store the template prompt answer
+                        this.templateConfig.template = answer.template;
+                });
+            },
+
+            // ask the user for final confirmation before we generate their bot
+            askForFinalConfirmation: () => {
+                if(noprompt) {
+                    return Promise.resolve();
+                }
+
+                return this.prompt({
+                    type: 'confirm',
+                    name: 'finalConfirmation',
+                    message: 'Looking good.  Shall I go ahead and create your new bot?',
+                    default: true
+                }).then(answer => {
+                    // store the finalConfirmation prompt answer
+                    this.templateConfig.finalConfirmation = answer.finalConfirmation;
+                });
+            }
+        };
+        return prompts;
+    }
+
+    // if we're run with the --noprompt option, verify that
+    // we were all passed in all required options.
+    // return true for success, false for failure
+    _verifyNoPromptOptions() {
+        this.templateConfig = _.pick(this.options, ['botName', 'packageName'])
+
+        // validate we have what we need, or we'll need to throw
+        if(!this.templateConfig.botName) {
+          throw new Error('Must specify a name for your bot when using --noprompt argument.  Use --botName or -N');
+        }
+        if(!this.templateConfig.packageName) {
+            throw new Error('Must specify a package name for your bot when using --noprompt argument.  Use --packageName or -P');
+        }
+
+        // make sure we have a supported template
+        const template = (this.templateConfig.template ? _.toLower(this.templateConfig.template) : undefined);
+        const tmplEmpty = _.toLower(BOT_TEMPLATE_NOPROMPT_EMPTY);
+        const tmplSimple = _.toLower(BOT_TEMPLATE_NOPROMPT_SIMPLE);
+        const tmplCore = _.toLower(BOT_TEMPLATE_NOPROMPT_CORE);
+        if (!template || (template !== tmplEmpty && template !== tmplSimple && template !== tmplCore)) {
+            throw new Error('Must specify a template when using --noprompt argument.  Use --template or -T');
+        }
+
+        // when run using --noprompt and we have all the required templateConfig, then set final confirmation to true
+        // so we can go forward and create the new bot without prompting the user for confirmation
+        this.templateConfig.finalConfirmation = true;
+    }
 };
