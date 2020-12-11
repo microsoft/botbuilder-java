35189c6616274e541ed30e24cc69efbb13e7e956
tracyboehrer <tracyboehrer@users.noreply.github.com>
2020-12-11 15:12:10 -0600

    Corrected units tests not running in generator templates

--- Generator/generator-botbuilder-java/generators/app/templates/echo/src/test/java/ApplicationTest.java
+++ Generator/generator-botbuilder-java/generators/app/templates/echo/src/test/java/ApplicationTest.java
@@ -10,7 +10,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ApplicationTests {
+public class ApplicationTest {
 
 	@Test
 	public void contextLoads() {
