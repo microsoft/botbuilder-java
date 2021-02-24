34ae2041365d52eb042d8b867079f0cbc82f92f3
Martin Battaglino <martinbatta32@gmail.com>
2021-02-24 19:23:55 -0300

    Update CosmosDBPartitionedStorage

--- libraries/bot-azure/src/main/java/com/microsoft/bot/azure/CosmosDbPartitionedStorage.java
+++ libraries/bot-azure/src/main/java/com/microsoft/bot/azure/CosmosDbPartitionedStorage.java
@@ -53,30 +53,43 @@ public class CosmosDbPartitionedStorage implements Storage {
      * Initializes a new instance of the CosmosDbPartitionedStorage class.
      * using the provided CosmosDB credentials, database ID, and container ID.
      *
-     * @param withStorageOptions Cosmos DB partitioned storage configuration options.
+     * @param withCosmosDbStorageOptions Cosmos DB partitioned storage configuration options.
      */
-    public CosmosDbPartitionedStorage(CosmosDbPartitionedStorageOptions withStorageOptions) {
-        if (withStorageOptions == null) {
-            throw new IllegalArgumentException("cosmosDbPartitionStorageOptions");
+    public CosmosDbPartitionedStorage(CosmosDbPartitionedStorageOptions withCosmosDbStorageOptions) {
+        if (withCosmosDbStorageOptions == null) {
+            throw new IllegalArgumentException("CosmosDbPartitionStorageOptions is required.");
         }
 
-        if (StringUtils.isEmpty(withStorageOptions.getCosmosDbEndpoint())) {
-            throw new IllegalArgumentException("cosmosDbEndpoint");
+        if (withCosmosDbStorageOptions.getCosmosDbEndpoint() == null) {
+            throw new IllegalArgumentException("Service EndPoint for CosmosDB is required: cosmosDbEndpoint");
         }
 
-        if (StringUtils.isEmpty(withStorageOptions.getAuthKey())) {
-            throw new IllegalArgumentException("authKey");
+        if (StringUtils.isBlank(withCosmosDbStorageOptions.getAuthKey())) {
+            throw new IllegalArgumentException("AuthKey for CosmosDB is required: authKey");
         }
 
-        if (StringUtils.isEmpty(withStorageOptions.getDatabaseId())) {
-            throw new IllegalArgumentException("databaseId");
+        if (StringUtils.isBlank(withCosmosDbStorageOptions.getDatabaseId())) {
+            throw new IllegalArgumentException("DatabaseId is required: databaseId");
         }
 
-        if (StringUtils.isEmpty(withStorageOptions.getContainerId())) {
-            throw new IllegalArgumentException("containerId");
+        if (StringUtils.isBlank(withCosmosDbStorageOptions.getContainerId())) {
+            throw new IllegalArgumentException("ContainerId is required: containerId");
         }
 
-        cosmosDbStorageOptions = withStorageOptions;
+        if (StringUtils.isNotBlank(withCosmosDbStorageOptions.getKeySuffix())) {
+            if (withCosmosDbStorageOptions.getCompatibilityMode()) {
+                throw new IllegalArgumentException("CompatibilityMode cannot be 'true' while using a KeySuffix: withCosmosDbStorageOptions");
+            }
+
+            // In order to reduce key complexity, we do not allow invalid characters in a KeySuffix
+            // If the KeySuffix has invalid characters, the EscapeKey will not match
+            String suffixEscaped = CosmosDbKeyEscape.escapeKey(withCosmosDbStorageOptions.getKeySuffix());
+            if (!withCosmosDbStorageOptions.getKeySuffix().equals(suffixEscaped)) {
+                throw new IllegalArgumentException(String.format("Cannot use invalid Row Key characters: %s %s", withCosmosDbStorageOptions.getKeySuffix(), "withCosmosDbStorageOptions"));
+            }
+        }
+
+        cosmosDbStorageOptions = withCosmosDbStorageOptions;
 
         objectMapper = new ObjectMapper()
             .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
@@ -93,9 +106,8 @@ public class CosmosDbPartitionedStorage implements Storage {
     /**
      * Reads storage items from storage.
      *
-     * @param keys keys of the items to read
-     * @return A task that represents the work queued to execute. If the activities
-     * are successfully sent, the task result contains the items read, indexed by key.
+     * @param keys A collection of Ids for each item to be retrieved.
+     * @return A dictionary containing the retrieved items.
      */
     @Override
     public CompletableFuture<Map<String, Object>> read(String[] keys) {
@@ -110,19 +122,19 @@ public class CosmosDbPartitionedStorage implements Storage {
 
         return getCollection()
             .thenApplyAsync(collection -> {
-                // issue all of the reads at once
+                // Issue all of the reads at once
                 List<CompletableFuture<Document>> documentFutures = new ArrayList<>();
                 for (String key : keys) {
-                    documentFutures.add(getDocumentById(CosmosDbKeyEscape.escapeKey(key)));
+                    documentFutures.add(getDocumentById(CosmosDbKeyEscape.escapeKey(key, cosmosDbStorageOptions.getKeySuffix(), cosmosDbStorageOptions.getCompatibilityMode())));
                 }
 
-                // map each returned Document to it's original value.
+                // Map each returned Document to it's original value.
                 Map<String, Object> storeItems = new HashMap<>();
                 documentFutures.forEach(documentFuture -> {
                     Document document = documentFuture.join();
                     if (document != null) {
                         try {
-                            // we store everything in a DocumentStoreItem.  Get that.
+                            // We store everything in a DocumentStoreItem. Get that.
                             JsonNode stateNode = objectMapper.readTree(document.toJson());
                             DocumentStoreItem storeItem = objectMapper.treeToValue(stateNode, DocumentStoreItem.class);
 
@@ -135,7 +147,7 @@ public class CosmosDbPartitionedStorage implements Storage {
                             }
                             storeItems.put(storeItem.getReadId(), item);
                         } catch (IOException | ClassNotFoundException e) {
-                            logger.warn("read", e);
+                            logger.warn("Error reading from container", e);
                         }
                     }
                 });
@@ -145,9 +157,10 @@ public class CosmosDbPartitionedStorage implements Storage {
     }
 
     /**
-     * Writes storage items to storage.
+     * Inserts or updates one or more items into the Cosmos DB container.
      *
-     * @param changes The items to write, indexed by key.
+     * @param changes A dictionary of items to be inserted or updated. The dictionary item key
+     * is used as the ID for the inserted / updated item.
      * @return A task that represents the work queued to execute.
      */
     @Override
@@ -156,7 +169,7 @@ public class CosmosDbPartitionedStorage implements Storage {
             throw new IllegalArgumentException("changes");
         }
 
-        if (changes.size() == 0) {
+        if (changes.isEmpty()) {
             return CompletableFuture.completedFuture(null);
         }
 
@@ -170,27 +183,28 @@ public class CosmosDbPartitionedStorage implements Storage {
                         // The ETag information is updated as an _etag attribute in the document metadata.
                         node.remove("eTag");
 
-                        DocumentStoreItem storeItem = new DocumentStoreItem() {{
-                            setId(CosmosDbKeyEscape.escapeKey(change.getKey()));
+                        DocumentStoreItem documentChange = new DocumentStoreItem() {{
+                            setId(CosmosDbKeyEscape.escapeKey(change.getKey(), cosmosDbStorageOptions.getKeySuffix(), cosmosDbStorageOptions.getCompatibilityMode()));
                             setReadId(change.getKey());
                             setDocument(node.toString());
                             setType(change.getValue().getClass().getTypeName());
                         }};
 
-                        Document document = new Document(objectMapper.writeValueAsString(storeItem));
+                        Document document = new Document(objectMapper.writeValueAsString(documentChange));
 
                         RequestOptions options = new RequestOptions();
-                        options.setPartitionKey(new PartitionKey(storeItem.partitionKey()));
+                        options.setPartitionKey(new PartitionKey(documentChange.partitionKey()));
 
                         if (change.getValue() instanceof StoreItem) {
-                            String eTag = ((StoreItem) change.getValue()).getETag();
-                            if (!StringUtils.isEmpty(eTag)) {
+                            String etag = ((StoreItem) change.getValue()).getETag();
+                            if (StringUtils.isNotBlank(etag) && !etag.equals("*")) {
+                                // if we have an etag, do opt. concurrency replace
                                 AccessCondition condition = new AccessCondition();
                                 condition.setType(AccessConditionType.IfMatch);
-                                condition.setCondition(eTag);
+                                condition.setCondition(etag);
 
                                 options.setAccessCondition(condition);
-                            } else if (eTag != null) {
+                            } else if (etag != null) {
                                 logger.warn("write change, empty eTag: " + change.getKey());
                                 continue;
                             }
@@ -203,7 +217,7 @@ public class CosmosDbPartitionedStorage implements Storage {
                             true);
 
                     } catch (JsonProcessingException | DocumentClientException e) {
-                        logger.warn("write change: " + change.getKey(), e);
+                        logger.warn("Error upserting document: " + change.getKey(), e);
                     }
                 }
 
@@ -212,9 +226,9 @@ public class CosmosDbPartitionedStorage implements Storage {
     }
 
     /**
-     * Deletes storage items from storage.
+     * Deletes one or more items from the Cosmos DB container.
      *
-     * @param keys keys of the items to delete
+     * @param keys An array of Ids for the items to be deleted.
      * @return A task that represents the work queued to execute.
      */
     @Override
@@ -226,7 +240,7 @@ public class CosmosDbPartitionedStorage implements Storage {
         // issue the deletes in parallel
         return getCollection()
             .thenCompose(collection -> Arrays.stream(keys).map(key -> {
-                String escapedKey = CosmosDbKeyEscape.escapeKey(key);
+                String escapedKey = CosmosDbKeyEscape.escapeKey(key, cosmosDbStorageOptions.getKeySuffix(), cosmosDbStorageOptions.getCompatibilityMode());
                 return getDocumentById(escapedKey)
                     .thenApplyAsync(document -> {
                         if (document != null) {
@@ -236,6 +250,7 @@ public class CosmosDbPartitionedStorage implements Storage {
 
                                 client.deleteDocument(document.getSelfLink(), options);
                             } catch (DocumentClientException e) {
+                                logger.warn("Unable to delete document", e);
                                 throw new CompletionException(e);
                             }
                         }
@@ -350,6 +365,7 @@ public class CosmosDbPartitionedStorage implements Storage {
      * Internal data structure for storing items in a CosmosDB Collection.
      */
     private static class DocumentStoreItem implements StoreItem {
+        // PartitionKey path to be used for this document type
         public static final String PARTITION_KEY_PATH = "/id";
 
         @JsonProperty(value = "id")
@@ -416,7 +432,7 @@ public class CosmosDbPartitionedStorage implements Storage {
         }
 
         /**
-         * Get eTag for concurrency.
+         * Get ETag information for handling optimistic concurrency updates.
          * @return The eTag value.
          */
         @Override
@@ -425,7 +441,7 @@ public class CosmosDbPartitionedStorage implements Storage {
         }
 
         /**
-         * Set eTag for concurrency.
+         * Set ETag information for handling optimistic concurrency updates.
          * @param withETag The eTag value.
          */
         @Override
