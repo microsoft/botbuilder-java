c344e9b1004ef8fa31bdd5eb5163bac546191082
Martin Battaglino <martinbatta32@gmail.com>
2021-03-05 16:58:53 -0300

    Add Startup file

---
+++ samples/13.core-bot/src/main/java/com/microsoft/bot/sample/core/Application.java
@@ -0,0 +1,114 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.microsoft.bot.sample.core;
+
+import com.microsoft.bot.builder.Bot;
+import com.microsoft.bot.builder.ConversationState;
+import com.microsoft.bot.builder.Storage;
+import com.microsoft.bot.builder.UserState;
+import com.microsoft.bot.dialogs.Dialog;
+import com.microsoft.bot.integration.AdapterWithErrorHandler;
+import com.microsoft.bot.integration.BotFrameworkHttpAdapter;
+import com.microsoft.bot.integration.Configuration;
+import com.microsoft.bot.integration.spring.BotController;
+import com.microsoft.bot.integration.spring.BotDependencyConfiguration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+
+/**
+ * This is the starting point of the Sprint Boot Bot application.
+ */
+@SpringBootApplication
+
+// Use the default BotController to receive incoming Channel messages. A custom
+// controller could be used by eliminating this import and creating a new
+// org.springframework.web.bind.annotation.RestController.
+// The default controller is created by the Spring Boot container using
+// dependency injection. The default route is /api/messages.
+@Import({BotController.class})
+
+/**
+ * This class extends the BotDependencyConfiguration which provides the default
+ * implementations for a Bot application.  The Application class should
+ * override methods in order to provide custom implementations.
+ */
+public class Application extends BotDependencyConfiguration {
+    /**
+     * The start method.
+     * @param args The args.
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    /**
+     * Returns the Bot for this application.
+     *
+     * <p>
+     *     The @Component annotation could be used on the Bot class instead of this method
+     *     with the @Bean annotation.
+     * </p>
+     *
+     * @return The Bot implementation for this application.
+     */
+    @Bean
+    public Bot getBot(
+    ) {
+        Storage storage = this.getStorage();
+        UserState userState = this.getUserState(storage);
+        ConversationState conversationState = this.getConversationState(storage);
+        Dialog rootDialog = this.getRootDialog();
+        return new DialogAndWelcomeBot(conversationState, userState, rootDialog);
+    }
+
+    /**
+     * Returns a FlightBookingRecognizer object.
+     * @return The FlightBookingRecognizer.
+     */
+    @Bean
+    public FlightBookingRecognizer getFlightBookingRecognizer() {
+        Configuration configuration = this.getConfiguration();
+        return new FlightBookingRecognizer(configuration);
+    }
+
+    /**
+     * Returns a BookingDialog object.
+     * @return The BookingDialog.
+     */
+    @Bean
+    public BookingDialog getBookingDialog() {
+        return new BookingDialog();
+    }
+
+    /**
+     * Returns the starting Dialog for this application.
+     *
+     * <p>
+     *     The @Component annotation could be used on the Dialog class instead of this method
+     *     with the @Bean annotation.
+     * </p>
+     *
+     * @return The Dialog implementation for this application.
+     */
+    @Bean
+    public Dialog getRootDialog() {
+        FlightBookingRecognizer flightBookingRecognizer = this.getFlightBookingRecognizer();
+        BookingDialog bookingDialog = this.getBookingDialog();
+        return new MainDialog(flightBookingRecognizer, bookingDialog);
+    }
+
+    /**
+     * Returns a custom Adapter that provides error handling.
+     *
+     * @param configuration The Configuration object to use.
+     * @return An error handling BotFrameworkHttpAdapter.
+     */
+    @Override
+    public BotFrameworkHttpAdapter getBotFrameworkHttpAdaptor(Configuration configuration) {
+        return new AdapterWithErrorHandler(configuration);
+    }
+}
+
